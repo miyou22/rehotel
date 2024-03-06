8721f4e2f62fee73b4b1c0eecd1293a19a24a0f7
miyou22 <87642297+miyou22@users.noreply.github.com>
2024-03-06 19:10:34 +0900

    feat: first commit - SpringBoot 프로젝트 빌드 - vue 레이아웃 적용 - 오라클 db 연동 - vue-springBoot 연동

---
+++ src/main/java/com/hotel/backend/controller/ItemController.java
@@ -0,0 +1,22 @@
+package com.hotel.backend.controller;
+
+import com.hotel.backend.entity.Item;
+import com.hotel.backend.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ItemController {
+    @Autowired
+    ItemRepository itemRepository;
+
+    @GetMapping("/api/items")
+    public List<Item> getItems(){
+        List<Item> items = itemRepository.findAll();
+
+        return items;
+    }
+}
