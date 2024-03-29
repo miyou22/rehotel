4d14ef0b62c1ebc804b991fa3650c9d05e3d0ace
macaron13 <eequalhv@gmail.com>
2024-03-29 13:42:33 +0900

    0329
    로그인 로그아웃 헤더 변경
    store.js수정
    로그인 스크립트 수정
    membercontroller, service 수정

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -67,10 +67,13 @@
               >
             </li> -->
 <!-- ------------------------------------------------------------------------------------------------------------------------- -->
+           <li v-if="!$store.state.userId"><a href="/login">LogIn</a></li>
+          <li v-else><a @click="logout()" style="cursor: pointer;">{{$store.state.userId}}님 Logout</a></li>
 
-            <li v-if="!isLoggedIn"><a href="/login">LogIn</a></li>
-            <li v-else><a @click="logout" style="cursor: pointer;">Logout</a></li>
-            <li v-if="!isLoggedIn"><a href="/joinmember">회원가입</a></li>
+
+
+
+            <li v-if="!$store.state.userId"><a href="/joinmember">회원가입</a></li>
             <li v-else><a href="/mypage">마이페이지</a></li>
             <li><a href="/checkList">예약확인</a></li>
             <li><a href="/location">오시는길</a></li>
@@ -118,6 +121,7 @@
 <script>
 // import toggle from "../assets/js/toggleHamburger";
 import axios from 'axios';
+import store from '@/store'
 const serverUrl = 'http://localhost:8081'
 
 export default {
@@ -144,59 +148,28 @@ export default {
       }
     },
   },
-   created() {
-      // 페이지가 생성될 때 로그인 상태를 확인하여 isLoggedIn 변수 업데이트
-      this.checkLoginStatus();
-    },
-  methods: {
-    toggle() {
-      const ul = document.querySelector(".header-nav-lists");
-      const hamburger = document.querySelector(".header-hamburger-icon");
-      ul.classList.toggle("show");
-      hamburger.classList.toggle("show");
-    },
-
-    async checkLoginStatus() {
-          try {
-            // 백엔드에 로그인 상태를 확인하는 요청을 보냄.
-            const response = await axios.get(`/api/member/checkLoginStatus`);
-            // 응답으로부터 로그인 상태를 가져와서 isLoggedIn 변수를 업데이트.
-            this.isLoggedIn = response.data.isLoggedIn;
-            console.log('로그인 상태:', this.isLoggedIn);
-          } catch (error) {
-            console.error('로그인 상태 확인 실패:', error);
-          }
-        },
-        async login() {
-          try {
-          // 로그인 요청을 보내고 성공하면 isLoggedIn을 true로 설정
-               await axios.post(`/api/member/login`, { userId: '사용자 아이디', userPwd: '비밀번호' });
-               this.$store.commit('setLoggedIn', true);
-               console.log('로그인 상태:', this.$store.state.isLoggedIn);
-
-               // 페이지를 새로고침
-               location.reload();
-              } catch (error) {
-               console.error('로그인 실패:', error);
-          }
-        },
-        async logout() {
-          try {
-            // 로그아웃 요청을 보내고 성공하면 isLoggedIn을 false로 설정
-           await axios.get(`/api/member/logout`);
-             this.$store.commit('setLoggedIn', false);
-             console.log('로그인 상태:', this.$store.state.isLoggedIn);
-
-             // 페이지를 새로고침
-            location.reload();
-           } catch (error) {
-             console.error('로그아웃 실패:', error);
-          }
-        }
-
+   methods: {
+      toggle() {
+        const ul = document.querySelector(".header-nav-lists");
+        const hamburger = document.querySelector(".header-hamburger-icon");
+        ul.classList.toggle("show");
+        hamburger.classList.toggle("show");
       },
+
+        logout() {
+          alert('로그아웃합니다');
+          // sessionStorage.removeItem("userId");
+            store.commit('setAccount', null)
+            localStorage.removeItem("sessionId");
+            console.log('세션제거 ==> ' + sessionStorage.sessionId)
+             this.$router.push({
+               path: '/',
+               name: 'home'
+           });
+        },
+    },
     };
-    </script>
+  </script>
 
 <style scoped>
 @import "../assets/css/Header.css";
